a784a3f650f41325d42b1fd8e69316e65816c8bd
Seth Yastrov <syastrov@gmail.com>
2013-12-16 10:44:41 +0100

    Added migration script to migrate vejledning's unit field to entity reference type.

---
+++ migrate_vejledning.php
@@ -0,0 +1,34 @@
+<?php
+
+define('DRUPAL_ROOT', getcwd());
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+migrateVejledningUnitToEntityReference();
+
+function migrateVejledningUnitToEntityReference()
+{
+    $sql = "SELECT entity_id FROM field_data_field_knowlegde_unit";
+    $result = db_query($sql);
+
+    foreach ($result as $n) {
+        $ids[] = $n->entity_id;
+    }
+
+    $nodes = node_load_multiple($ids);
+
+    foreach($nodes as $node) {
+        foreach ($node->field_knowlegde_unit[LANGUAGE_NONE] as $i => $tid) {
+          $node->field_knowlegde_unit_ref[LANGUAGE_NONE][]['target_id'] = $tid;
+        }
+//        $sql = "INSERT INTO field_data_field_knowlegde_unit_ref VALUES (entity_type, bundle, entity_id, revision_id, language, delta, field_data_field_knowlegde_unit_ref_target_id)"
+//          . " (:entity_type, :bundle, :entity_id, :revision_id, :language, :delta, :val)";
+//        $res = db_query($sql,array(':entity_type' => $node->entity_type, ':bundle' => $node->bundle, ':entity_id' => $node->entity_id,
+//          ':revision_id' => 1, ':language' => $node->language, ':delta' => $node->delta,
+//          ':val' => $node->field_knowlegde_unit[LANGUAGE_NONE]));
+
+        print_r($node);
+        field_attach_update('node', $node);
+    }
+}
+
