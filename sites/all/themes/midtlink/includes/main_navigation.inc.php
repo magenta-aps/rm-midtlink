b8e08edba263460e77c3ec57700e64278f579f3a
Seth Yastrov <syastrov@gmail.com>
2014-01-08 14:29:57 +0100

    Fixed #9001: "Tags forsvinder" When viewing /taxonomy/term/.. URL, Forum tab is not active.

--- sites/all/themes/midtlink/includes/main_navigation.inc.php
+++ sites/all/themes/midtlink/includes/main_navigation.inc.php
@@ -11,7 +11,7 @@
   if($is_front) { $homeActive = ' active'; }
   if(arg(0) == 'obssheet' || arg(0) == 'create_observation') { $obsActive = ' active'; }
   if(isset($node) && $node->type == 'post') { $forumActive = ' active'; }
-  if(arg(0) == 'forum' || arg(0) == 'browse_categories') { $forumActive = ' active'; }
+  if(arg(0) == 'forum' || arg(0) == 'browse_categories' || arg(0) == 'taxonomy') { $forumActive = ' active'; }
   if(arg(0) == 'dokumentation') { $docuActive = ' active'; }
   if(isset($node) && $node->type == 'knowlegde') { $docuActive = ' active'; }
 ?>
