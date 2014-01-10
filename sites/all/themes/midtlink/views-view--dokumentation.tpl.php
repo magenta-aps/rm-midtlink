ab1e9272233043a276066e6af4112c09d101e180
Seth Yastrov <syastrov@gmail.com>
2014-01-10 14:10:43 +0100

    Remove Dropdown on Vejledning tab.

--- sites/all/themes/midtlink/views-view--dokumentation.tpl.php
+++ sites/all/themes/midtlink/views-view--dokumentation.tpl.php
@@ -1,32 +1,11 @@
-	<div class="documentation actions" style="height:80px;">
+	<!--div class="documentation actions" style="height:80px;">
     <?php //if (user_access('create knowlegde content')) {
         //echo '<p><a href="' . url('node/add/knowlegde/') . '">Opret Vejledning</a></p>';
       //}; ?>
     
-    <?php include('includes/unit_tabs.inc.php'); ?>		
-		<div class="sort">
-			<form action="#">
-			<label for="select-choice">Vis kun</label>
-			<select name="select-choice" id="select-choice" onchange="location.href=this.options[this.selectedIndex].value">
-				<option value="/dokumentation">- Alle -</option>
-				<?php
-        
-        $current_tid = arg(1);
-        $local_term = taxonomy_term_load($current_tid);
-        if ($local_term) {
-  				$categories = midtlink_get_local_and_global_keywords($local_term->name);
-        } else {
-          $categories = midtlink_get_global_keywords();
-        }
-        
-				foreach($categories as $i) {
-					echo '<option value="/dokumentation/'.arg(1).'/'.$i->tid.'"'.($i->tid == arg(1) ? ' selected="selected"' : '').'>'.$i->name.'</option>'."\n";
-				}
-				?>
-			</select>
-			</form>
-		</div>
-	</div>
+    
+	</div-->
+  <?php include('includes/unit_tabs.inc.php'); ?>		
 	<?php
   if(arg(2) != '') {
 		$term = taxonomy_term_load(arg(2));
