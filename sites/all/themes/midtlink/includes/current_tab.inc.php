566a6595312ff229adb57652d46309fe49887d15
Seth Yastrov <syastrov@gmail.com>
2014-01-13 13:10:28 +0100

    Oversigter: Sidebar shows local Emne for currently selected unit.

--- sites/all/themes/midtlink/includes/current_tab.inc.php
+++ sites/all/themes/midtlink/includes/current_tab.inc.php
@@ -10,6 +10,11 @@
     $activeMainTID = arg(1);
     $activeTermTID = arg(2);
   }
+  else if (arg(0) == 'obssheet') {
+    $activeMainTID = arg(2);
+    $activeMainTID = midtlink_get_main_unit_from_subunit($activeMainTID)->tid;
+    $activeTermTID = intval($_GET['filter_category']);
+  }
   else {
     global $user;
     $activeMainTID = $user->mainUnitTID;
