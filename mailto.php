5d6a1f343fd30b8e54f6b2e9534acd790cef195f
Seth Yastrov <syastrov@gmail.com>
2014-01-21 15:13:25 +0100

    Trying to fix mailto links.

--- mailto.php
+++ mailto.php
@@ -1,14 +1,14 @@
 <?php
-header('Content-type:text/html;charset=iso-8859-1');
+header('Content-type:text/html;charset=utf-8');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="da" version="XHTML+RDFa 1.0" dir="ltr">
 <head profile="http://www.w3.org/1999/xhtml/vocab">	
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
 <script type="text/javascript">
-location.href='mailto:?<?php echo rawurlencode(utf8_decode($_GET['e'])); ?>&subject=<?php echo rawurlencode(utf8_decode($_GET['s'])); ?>&body=<?php echo rawurlencode(utf8_decode($_GET['b'])); ?>';
+location.href='mailto:?<?php echo rawurlencode($_GET['e']); ?>&subject=<?php echo rawurlencode($_GET['s']); ?>&body=<?php echo rawurlencode($_GET['b']); ?>';
 </script>
 </body>
 </html>
