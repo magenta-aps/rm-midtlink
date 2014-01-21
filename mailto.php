c450c3d675203e81a44c58594158347cfa6e54fe
Seth Yastrov <syastrov@gmail.com>
2014-01-21 15:51:27 +0100

    charset

--- mailto.php
+++ mailto.php
@@ -1,10 +1,10 @@
 <?php
-header('Content-type:text/html;charset=utf-8');
+header('Content-type:text/html;charset=iso-8859-1');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="da" version="XHTML+RDFa 1.0" dir="ltr">
 <head profile="http://www.w3.org/1999/xhtml/vocab">	
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
 <body>
   <a href="mailto:?<?php echo htmlentities(utf8_decode($_GET['e'])); ?>&amp;subject=<?php echo htmlentities(utf8_decode($_GET['s'])); ?>&amp;body=<?php echo htmlentities(utf8_decode($_GET['b'])); ?>">test</a>
