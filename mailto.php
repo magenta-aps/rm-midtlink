4c285d8656fa4119511abb46c988815cf4fa60c7
Seth Yastrov <syastrov@gmail.com>
2014-01-21 15:57:42 +0100

    lf2entities

--- mailto.php
+++ mailto.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-type:text/html;charset=iso-8859-1');
+function lf2entities($subject) {
+  return str_replace('\n', '&#10;', $subject);
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="da" version="XHTML+RDFa 1.0" dir="ltr">
@@ -7,7 +10,7 @@ header('Content-type:text/html;charset=iso-8859-1');
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 </head>
 <body>
-  <a href="mailto:?<?php echo htmlentities(utf8_decode($_GET['e'])); ?>&amp;subject=<?php echo htmlentities(utf8_decode($_GET['s'])); ?>&amp;body=<?php echo htmlentities(utf8_decode($_GET['b'])); ?>">test</a>
+  <a href="mailto:?<?php echo htmlentities(utf8_decode($_GET['e'])); ?>&amp;subject=<?php echo htmlentities(utf8_decode($_GET['s'])); ?>&amp;body=<?php echo htmlentities(utf8_decode(lf2entities($_GET['b']))); ?>">test</a>
 </script>
 
 </body>
