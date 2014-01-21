372a9d081f0f63ed0a09e6202fd3033979418815
Seth Yastrov <syastrov@gmail.com>
2014-01-21 11:45:54 +0100

    Updated migrate Vejledning script to set all current Vejledninger's unit to AUH. Fixed links in Oversigter PDF output.

--- migrate_vejledning.php
+++ migrate_vejledning.php
@@ -8,20 +8,20 @@ migrateVejledningUnitToEntityReference();
 
 function migrateVejledningUnitToEntityReference()
 {
-    $sql = "SELECT entity_id FROM field_data_field_knowlegde_unit";
+//    $sql = "SELECT entity_id FROM field_data_field_knowlegde_unit";
+  $sql = "select nid from node where type = 'knowlegde';";
     $result = db_query($sql);
 
     foreach ($result as $n) {
-        $ids[] = $n->entity_id;
+        $ids[] = $n->nid;
     }
 
     $nodes = node_load_multiple($ids);
 
     foreach($nodes as $node) {
         $node->field_unit[LANGUAGE_NONE] = array();
-        foreach ($node->field_knowlegde_unit[LANGUAGE_NONE] as $i => $val) {
-          $node->field_unit[LANGUAGE_NONE][]['tid'] = $val['value'];
-        }
+        $node->field_unit[LANGUAGE_NONE][]['tid'] = 300; // set to AUH
+        
 //        $sql = "INSERT INTO field_data_field_knowlegde_unit_ref VALUES (entity_type, bundle, entity_id, revision_id, language, delta, field_data_field_knowlegde_unit_ref_target_id)"
 //          . " (:entity_type, :bundle, :entity_id, :revision_id, :language, :delta, :val)";
 //        $res = db_query($sql,array(':entity_type' => $node->entity_type, ':bundle' => $node->bundle, ':entity_id' => $node->entity_id,
