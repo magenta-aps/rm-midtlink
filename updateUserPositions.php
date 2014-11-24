277f430ad05e207898fe00b81628b7291d98c13b
root <root@exrhel0001.it.rm.dk>
2014-11-24 15:07:04 +0100

    Issue #11514: Added updating of user full names.

--- updateUserPositions.php
+++ updateUserPositions.php
@@ -18,3 +18,18 @@ foreach($res as $r) {
 	echo 'UPDATE '.$r->name.': '.$r->position."\n";
 }
 
+// Update user's full names (from all time), ONLY if it is changed
+
+$sql = "SELECT u.name, u.uid, b.display_name FROM users u
+        INNER JOIN {bsk_user_data} b ON b.user_id = u.name INNER JOIN
+        {field_data_field_fullname} f ON f.entity_id = u.uid AND f.bundle = 'user'
+        AND b.display_name != '' AND b.display_name != f.field_fullname_value
+        WHERE u.status = 1";  // AND b.updated >= ".(time()-108000);
+$res = db_query($sql)->fetchAll();
+
+foreach($res as $r) {
+  $sql = "UPDATE {field_data_field_fullname} SET field_fullname_value = :fullname
+                WHERE entity_id = :uid AND bundle = 'user' AND entity_type = 'user'";
+  db_query($sql,array(':fullname'=>$r->display_name,':uid'=>$r->uid));
+  echo 'UPDATE '.$r->name.': '.$r->display_name."\n";
+}
